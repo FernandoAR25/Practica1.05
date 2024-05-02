70dfe68f638be16801b5aecba94382d67cb1cf6d
fernando <farenasr2511@gmail.com>
2024-05-01 23:34:32 -0600

    commit final

--- practica105.js
+++ practica105.js
@@ -19,19 +19,19 @@ app.use(express.static(path.join(__dirname,'public')));
 
 //GET Principal
 app.get('/', (req, res) => {
-    res.send('Raiz principal');
+    res.send('Metodo get ejecutado correctamente');
     
 });
 
 //POST en la ruta raíz ('/') - ver con postman
 app.post('/', (req, res) => {
-    res.send('Raiz post');
+    res.send('Metodo post ejecutado correctamente');
     
 });
 
 //PUT en la ruta raíz ('/') - ver con postman 
 app.put('/', (req, res) => {
-    res.send('Hola, soy el método PUT');
+    res.send('Metodo put ejecutado correctamente');
 });
 
 //Prueba error 500
@@ -49,7 +49,7 @@ const fotos = {
 }
 
 //GET fotos aleatorias
-app.get('/ruta-1',(req,res) => {
+app.get('/ruta1',(req,res) => {
     const llaves = Object.keys(fotos);
     res.render('rutas/ruta1',{foto: fotos[llaves[llaves.length*Math.random() << 0]]});
     
@@ -58,27 +58,27 @@ app.get('/ruta-1',(req,res) => {
 const Aleatorio = () => Math.floor(Math.random() * 1000) + 1;
 
 //Ejercicios anteriores - aleatorio
-app.get('/ruta-2', (req, res) => {
+app.get('/ruta2', (req, res) => {
     const num = Aleatorio();
     res.render('rutas/ruta2',{num});
 });
 
 //Ejercicios anteriores - texto plano
-app.get('/ruta-3', (req, res) => {
-    const texto = 'Hola, soy texto plano';
+app.get('/ruta3', (req, res) => {
+    const texto = 'Ejecución de texto de fernando y haniel';
     res.render('rutas/ruta3',{texto});
 });
 
-//Middleware para manejar el error 404
-app.use((req, res) => {
-    res.status(404);
-    res.render('error/404');
-});
 
 // Middleware para manejar el error 500
+//app.use((err, req, res, next) => {
+    //console.error(err.stack);
+    //res.status(500).render('error/500'); // Enviar la respuesta al cliente
+//});
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).render('error/500'); // Enviar la respuesta al cliente
+    res.status(404).render('error/404'); // Enviar la respuesta al cliente
 });
 
 app.use(express.json());
